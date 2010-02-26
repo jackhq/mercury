5a37776c5df7c44efb9a12560786e4cf0f1661ba
Tom & Russell <thing2@jackhq.com>
2010-02-26 17:05:23 -0500

    fixed layout issue

--- lib/mercury.rb
+++ lib/mercury.rb
@@ -18,38 +18,38 @@ end
 __END__
 
   @@ layout
-  !!!
-  %html{ :xmlns => "http://www.w3.org/1999/xhtml", :lang => "en", 'xml:lang' => "en" }
-    %head
-      %title Mercury
-      - ['reset','text','960','app'].each do |css|
-        %link{ :href => "/stylesheets/#{css}.css", :rel => "stylesheet", :type => "text/css", :media => "screen", :charset => "utf-8" }
-    %body
-      #header
-        .container_12
-          %small{:style => 'font-style:italic;float:right;'} WireFrame Tool
-          %h1{:style => 'margin:0;padding:0;'}
-            %a{:href => '/', :style => 'color:white;text-decoration:none;'} Mercury
+!!!
+%html{ :xmlns => "http://www.w3.org/1999/xhtml", :lang => "en", 'xml:lang' => "en" }
+  %head
+    %title Mercury
+    - ['reset','text','960','app'].each do |css|
+      %link{ :href => "/stylesheets/#{css}.css", :rel => "stylesheet", :type => "text/css", :media => "screen", :charset => "utf-8" }
+  %body
+    #header
+      .container_12
+        %small{:style => 'font-style:italic;float:right;'} WireFrame Tool
+        %h1{:style => 'margin:0;padding:0;'}
+          %a{:href => '/', :style => 'color:white;text-decoration:none;'} Mercury
 
-      #wrap
-        .container_16
-          = yield
-      .clear
-      #footer
-        .container_12
-          .grid_4.prefix_8
-            %p Jack Russell Software Company, LLC
+    #wrap
+      .container_16
+        = yield
+    .clear
+    #footer
+      .container_12
+        .grid_4.prefix_8
+          %p Jack Russell Software Company, LLC
 
-      - ['jquery-1.4.2.min'].each do |js|
-        %script{:src => "/javascripts/#{js}.js", :type => 'text/javascript'}
+    - ['jquery-1.4.2.min'].each do |js|
+      %script{:src => "/javascripts/#{js}.js", :type => 'text/javascript'}
 
-  @@ index
-  .grid_16
-    %p Welcome
+@@ index
+.grid_16
+  %p Welcome
 
-    %ul
-      - Dir.glob('views/**/*.haml').each do |f|
-        - unless f =~ /index/ or f =~ /layout/
-          %li
-            %a{:href => f.gsub(/views/,'').match(/(.*).haml$/)[1]}= f.match(/\/(.*).haml$/)[1]
+  %ul
+    - Dir.glob('views/**/*.haml').each do |f|
+      - unless f =~ /index/ or f =~ /layout/
+        %li
+          %a{:href => f.gsub(/views/,'').match(/(.*).haml$/)[1]}= f.match(/\/(.*).haml$/)[1]
 
