de683b8b56c05fea684cc2344abc4724968a8add
Tom & Russell <thing2@jackhq.com>
2010-03-05 14:02:45 -0500

    updated sass and js includes so you don't have to specify path or ext

--- lib/mercury.rb
+++ lib/mercury.rb
@@ -9,20 +9,28 @@ class Mercury < Sinatra::Application
   set :public, File.dirname(__FILE__) + '/public'
 
   get '/*' do
-    unless params["splat"][0].empty?
-      haml params["splat"][0].to_sym, :layout => open(File.dirname(__FILE__) + '/views/layout.haml','r').read
-    else
-      haml open(File.dirname(__FILE__) + '/views/index.haml','r').read, :layout => open(File.dirname(__FILE__) + '/views/layout.haml','r').read
-    end
+    layout_template = open(File.dirname(__FILE__) + '/views/layout.haml','r').read
+    view_file = params["splat"][0].empty? ? params["splat"][0].to_sym : open(File.dirname(__FILE__) + '/views/index.haml','r').read
+    haml view_file, :layout => layout_template
   end
 
   def sass(sassfile)
-    "<style type='text/css'>\n" + Sass::Engine.new(open(options.views + sassfile, 'r').read).render + "\n</style>\n"
+    ["<style type='text/css'>",
+      Sass::Engine.new(open(find_file(sassfile, '.sass'), 'r').read).render, 
+      "</style>\n".join("\n")
   end
   
   def javascript(jsfile)
-    "<script type='text/javascript'>\n" + open(options.views + jsfile, 'r').read + "\n</script>\n"
+    ["<script type='text/javascript'>", 
+      open(find_file(jsfile,'js'), 'r').read, 
+      "</script>\n"].join("\n")
   end
+  
+private  
+  def find_file(filename, ext)
+    Dir.glob(File.join(options.view, "**/*.#{ext}")).select { |f| f =~ /#{filename}.#{ext}$/ }.first
+  end
+  
   
   
 end
