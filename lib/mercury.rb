43c32e750f58c273e5e2266178418eb75b28ce78
Tom & Russell <thing2@jackhq.com>
2010-03-05 14:33:39 -0500

    fixed bug with sass

--- lib/mercury.rb
+++ lib/mercury.rb
@@ -5,25 +5,31 @@ require 'fileutils'
 require 'faker'
 
 class Mercury < Sinatra::Application
+  SASS = 'sass'
+  JS = 'js'
+  
   set :root,  FileUtils.pwd.gsub("\n",'')
   set :public, File.dirname(__FILE__) + '/public'
 
   get '/*' do
+    vf = params["splat"][0]
     vd = File.join(File.dirname(__FILE__),'views')
     layout_template = open(File.join(vd,'layout.haml'),'r').read
-    view_file = params["splat"][0].empty? ? open(File.join(vd,'index.haml'),'r').read : params["splat"][0].to_sym
+
+    view_file = vf.empty? ? open(File.join(vd,'index.haml'),'r').read : vf.to_sym
+
     haml view_file, :layout => layout_template
   end
 
   def sass(sassfile)
     ["<style type='text/css'>",
-      Sass::Engine.new(open(find_file(sassfile, '.sass'), 'r').read).render, 
+      Sass::Engine.new(open(find_file(sassfile, SASS), 'r').read).render, 
       "</style>\n"].join("\n")
   end
   
   def javascript(jsfile)
     ["<script type='text/javascript'>", 
-      open(find_file(jsfile,'js'), 'r').read, 
+      open(find_file(jsfile, JS), 'r').read, 
       "</script>\n"].join("\n")
   end
   
@@ -31,7 +37,5 @@ private
   def find_file(filename, ext)
     Dir.glob(File.join(options.views, "**/*.#{ext}")).select { |f| f =~ /#{filename}.#{ext}$/ }.first
   end
-  
-  
   
 end
